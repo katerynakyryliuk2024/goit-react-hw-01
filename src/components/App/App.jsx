c214348a8b0ccf18bf2f308866e79ca02aa2c605
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-02-21 17:24:21 +0300

    add userData component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,20 +1,9 @@
 //import { useState } from "react";//
 
-import "./App.css";
+import css from "./App.module.css";
 
 import Profile from "../Profile/Profile";
-
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308,
-  },
-};
+import userData from "../userData.json";
 
 export default function App() {
   return (
