5f3a76013f30a8bff77cb721dcc557340e5a50a4
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-02-21 16:55:52 +0300

    add new components

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+//import { useState } from "react";//
+
+import "./App.css";
+
+import Profile from "../Profile/Profile";
+
+const userData = {
+  username: "Jacques Gluke",
+  tag: "jgluke",
+  location: "Ocho Rios, Jamaica",
+  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+export default function App() {
+  return (
+    <>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+    </>
+  );
+}
