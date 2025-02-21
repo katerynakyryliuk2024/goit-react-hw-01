735775e4d200e3385e38016c7ed6d35e69a2b1f1
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-02-21 16:28:28 +0300

    add modern normalize

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,24 @@
-import { useState } from "react";
+//import { useState } from "react";//
 
 import "./App.css";
 
-const favBooks = [
-  { id: "id-1", name: "JS for beginners" },
-  { id: "id-2", name: "React basics" },
-  { id: "id-3", name: "React Router overview" },
-];
-
-const BookList = ({ books }) => {
-  return (
-    <ul>
-      {books.map((book) => {
-        return <li key={book.id}>{book.name}</li>;
-      })}
-    </ul>
-  );
-};
+import Alert from "./Alert";
 
 export default function App() {
   return (
     <>
-      <h1>Books of the week</h1>
-      <BookList books={favBooks} />
+      <Alert variant="info">
+        Would you like to browse our recommended products?
+      </Alert>
+      <Alert variant="error">
+        There was an error during your last transaction
+      </Alert>
+      <Alert variant="success">
+        Payment received, thank you for your purchase
+      </Alert>
+      <Alert variant="warning">
+        Please update your profile contact information
+      </Alert>
     </>
   );
 }
