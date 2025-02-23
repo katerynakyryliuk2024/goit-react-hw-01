a981e01a486cdc87bd9ae50e556edd028632e4c2
Kateryna Kyryliuk <katerynaerdek@gmail.com>
2025-02-23 19:56:36 +0300

    update TransactionHistory

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -3,11 +3,11 @@ import css from "./TransactionHistory.module.css";
 export default function TransactionHistory({ items }) {
   return (
     <table className={css.container}>
-      <thead>
+      <thead className={css.head}>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={css.headEl}>Type</th>
+          <th className={css.headEl}>Amount</th>
+          <th className={css.headEl}>Currency</th>
         </tr>
       </thead>
 
@@ -15,7 +15,7 @@ export default function TransactionHistory({ items }) {
         {items.map((item) => {
           console.log(item);
           <tr key={item.id}>
-            <td>{item.type}</td>
+            <td className={css.cell}>{item.type}</td>
             <td>{item.amount}</td>
             <td>{item.currency}</td>
           </tr>;
